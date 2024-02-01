86a03b7f712155aa36c9bf639227e824d7e47d98
Sergii Kotenko <ksv.root@gmail.com>
2024-02-01 18:18:47 +0200

    Tables are created

--- rest/application/pgdb.py
+++ rest/application/pgdb.py
@@ -1,11 +1,10 @@
 import psycopg2
 import os
 
-def connect():
+def conn():
     return psycopg2.connect(
-        host=getenv('POSTGRES_HOST'),
-        port=getenv('POSTGRES_PORT'),
-        dbname=getenv('POSTGRES_DB'),
-        user=getenv('POSTGRES_USER'),
-        password=getenv('POSTGRES_PASSWORD'))
-    
+        host=os.getenv('POSTGRES_HOST'),
+        port=os.getenv('POSTGRES_PORT'),
+        dbname=os.getenv('POSTGRES_DB'),
+        user=os.getenv('POSTGRES_USER'),
+        password=os.getenv('POSTGRES_PASSWORD'))
